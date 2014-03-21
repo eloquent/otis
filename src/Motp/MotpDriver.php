81c5070f8d9354ff8f400372dc2975761945cb5a
Erin Millard <ezzatron@gmail.com>
2014-03-21 14:50:30 +1000

    Copyright year bump.

--- src/Motp/MotpDriver.php
+++ src/Motp/MotpDriver.php
@@ -3,10 +3,10 @@
 /*
  * This file is part of the Otis package.
  *
- * Copyright © 2013 Erin Millard
+ * Copyright © 2014 Erin Millard
  *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
  */
 
 namespace Eloquent\Otis\Motp;
